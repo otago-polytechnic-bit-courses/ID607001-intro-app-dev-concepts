041a7564945880d26a53f0a853f0043702148fe5
grayson-orr <graysono@op.ac.nz>
2021-11-05 08:45:00 +1300

    Update

---
+++ lecture-notes/graysono-playground/react-example/src/AppBackup.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Counter from './components/Counter'
+import Lifecycle from './components/Lifecycle'
+import Post from './components/Post'
+import Welcome from './components/Welcome'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Welcome /> {/* This will render - Welcome an <h1> element. Note: a prop attribute is not given */}
+      <Welcome firstName="John" /> {/* 
+                                     It will render - Welcome John in an <h1> element. Note: a prop 
+                                     attribute is given. Make sure the prop attribute's name is the
+                                     same as what is specified in the child component, i.e., Welcome.js. 
+                                   */}
+      <Counter />
+      <Post />
+      <Lifecycle />
+    </div>
+  )
+}
+
+export default App
