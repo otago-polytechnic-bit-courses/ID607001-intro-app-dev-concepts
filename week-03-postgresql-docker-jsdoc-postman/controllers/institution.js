8ba63445e9e7ff8f960984743a6ad7133ad54c8c
Grayson Orr <grayson.orr@op.ac.nz>
2025-07-29 10:20:46 +1200

    update

---
+++ week-03-postgresql-docker-jsdoc-postman/controllers/institution.js
@@ -0,0 +1,141 @@
+import prisma from "../prisma/client.js";
+
+const createInstitution = async (req, res) => {
+  // Try/catch blocks are used to handle exceptions
+  try {
+    // Create a new institution
+    await prisma.institution.create({
+      // Data to be inserted
+      data: {
+        name: req.body.name,
+        region: req.body.region,
+        country: req.body.country,
+      },
+    });
+
+    // Get all institutions from the institution table
+    const newInstitutions = await prisma.institution.findMany();
+
+    // Send a JSON response
+    return res.status(201).json({
+      message: "Institution successfully created",
+      data: newInstitutions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getInstitutions = async (req, res) => {
+  try {
+    const institutions = await prisma.institution.findMany();
+
+    // Check if there are no institutions
+    if (!institutions) {
+      return res.status(404).json({ message: "No institutions found" });
+    }
+
+    return res.status(200).json({
+      data: institutions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getInstitution = async (req, res) => {
+  try {
+    const institution = await prisma.institution.findUnique({
+      where: { id: req.params.id },
+    });
+
+    // Check if there is no institution
+    if (!institution) {
+      return res.status(404).json({
+        message: `No institution with the id: ${req.params.id} found`,
+      });
+    }
+
+    return res.status(200).json({
+      data: institution,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const updateInstitution = async (req, res) => {
+  try {
+    // Find the institution by id
+    let institution = await prisma.institution.findUnique({
+      where: { id: req.params.id },
+    });
+
+    // Check if there is no institution
+    if (!institution) {
+      return res.status(404).json({
+        message: `No institution with the id: ${req.params.id} found`,
+      });
+    }
+
+    // Update the institution
+    institution = await prisma.institution.update({
+      where: { id: req.params.id },
+      data: {
+        // Data to be updated
+        name: req.body.name,
+        region: req.body.region,
+        country: req.body.country,
+      },
+    });
+
+    return res.status(200).json({
+      message: `Institution with the id: ${req.params.id} successfully updated`,
+      data: institution,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const deleteInstitution = async (req, res) => {
+  try {
+    const institution = await prisma.institution.findUnique({
+      where: { id: req.params.id },
+    });
+
+    if (!institution) {
+      return res.status(404).json({
+        message: `No institution with the id: ${req.params.id} found`,
+      });
+    }
+
+    await prisma.institution.delete({
+      where: { id: req.params.id },
+    });
+
+    return res.json({
+      message: `Institution with the id: ${req.params.id} successfully deleted`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+export {
+  createInstitution,
+  getInstitutions,
+  getInstitution,
+  updateInstitution,
+  deleteInstitution,
+};
