7cd4c01c1f39f231cff54dbdb4ff8c3bfda7a3dc
graysono <graysono@op.ac.nz>
2025-08-06 17:15:27 +1200

    update

--- week-06-authentication-rbac-api-testing/routes/institution.js
+++ week-06-authentication-rbac-api-testing/routes/institution.js
@@ -8,16 +8,27 @@ import {
   deleteInstitution,
 } from "../controllers/institution.js";
 
+import {
+  validatePostInstitution,
+  validatePutInstitution,
+} from "../middleware/validation/institution.js";
+
 import jwtAuth from "../middleware/jwtAuth.js";
 
 import rbac from "../middleware/rbac.js";
 
 const router = express.Router();
 
-router.post("/", jwtAuth, rbac("ADMIN"), createInstitution);
+router.post(
+  "/",
+  jwtAuth,
+  validatePostInstitution,
+  rbac("ADMIN"),
+  createInstitution
+);
 router.get("/", getInstitutions);
 router.get("/:id", getInstitution);
-router.put("/:id", updateInstitution);
+router.put("/:id", validatePutInstitution, updateInstitution);
 router.delete("/:id", deleteInstitution);
 
 export default router;
