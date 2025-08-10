4240b51054d3ac213837a0bfaf4a66626b9fd10d
graysono <graysono@op.ac.nz>
2025-08-11 09:56:37 +1200

    update

---
+++ week-08-content-delivery-networks-api-integration-1/src/routes/server-side/express-api/+page.server.js
@@ -0,0 +1,49 @@
+// /routes/server-side/express-api/+page.server.js
+
+import { env } from '$env/dynamic/private';
+
+const API_BASE_URL = env.API_BASE_URL || 'http://localhost:3000';
+
+export const load = async () => {
+	try {
+		const res = await fetch(`${API_BASE_URL}/api/institutions`);
+		const institutions = await res.json();
+
+		return {
+			institutions,
+			error: null
+		};
+	} catch (err) {
+		return {
+			institutions: [],
+			error: err.message
+		};
+	}
+};
+
+export const actions = {
+	create: async ({ request }) => {
+		const data = await request.formData();
+		const name = data.get('name');
+		const region = data.get('region');
+		const country = data.get('country');
+		const institution = { name, region, country };
+
+		try {
+			const res = await fetch(`${API_BASE_URL}/api/institutions`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(institution)
+			});
+
+			const institutions = await res.json();
+
+			return { success: true, message: institutions.message };
+		} catch (err) {
+			console.log(err);
+			return { success: false, error: err.message };
+		}
+	}
+};
---
+++ week-08-content-delivery-networks-api-integration-1/src/routes/server-side/express-api/+page.svelte
@@ -0,0 +1,41 @@
+<!-- /server-side/express-api/+page.svelte -->
+
+<script>
+	let { data, form } = $props();
+	let institutions = data.institutions.data;
+	let error = data.error;
+</script>
+
+<form method="POST" action="?/create">
+	<label for="name">Name:</label>
+	<input id="name" name="name" type="text" placeholder="Enter name" />
+
+	<label for="region">Region:</label>
+	<input id="region" name="region" type="text" placeholder="Enter region" />
+
+	<label for="country">Country:</label>
+	<input id="country" name="country" type="text" placeholder="Enter country" />
+
+	<button type="submit">Submit</button>
+</form>
+
+{#if form?.success}
+	<p>{form.message}</p>
+{/if}
+
+{#if form?.success === false}
+	<p>{form.error}</p>
+{/if}
+
+{#if error}
+	<p>{error}</p>
+{:else if institutions.length > 0}
+	<h1>Institutions</h1>
+	<ul>
+		{#each institutions as institution}
+			<li>{institution.name}</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No institutions found</p>
+{/if}
