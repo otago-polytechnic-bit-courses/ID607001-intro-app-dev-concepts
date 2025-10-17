5ddb80a85430fa7300618b188749010f8d4e862e
Grayson Orr <grayson.orr@op.ac.nz>
2025-10-17 22:23:46 +1300

    update

---
+++ lecture-notes-examples/week-05-validation-seeding-query-parameters-deployment/controllers/institution.js
@@ -0,0 +1,136 @@
+import institutionRepository from "../repositories/institution.js";
+
+const createInstitution = async (req, res) => {
+  try {
+    const { name, region, country } = req.body;
+    await institutionRepository.create({ name, region, country });
+    const newInstitutions = await institutionRepository.findAll();
+    return res.status(201).json({
+      message: "Institution successfully created",
+      data: newInstitutions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getInstitutions = async (req, res) => {
+  try {
+    const {
+      name,
+      region,
+      country,
+      sortBy = "id",
+      sortOrder = "asc",
+      page = 1,
+      pageSize = 10,
+    } = req.query;
+
+    const filters = {};
+    if (name) filters.name = name;
+    if (region) filters.region = region;
+    if (country) filters.country = country;
+
+    const validSortOrders = ["asc", "desc"];
+    const order = validSortOrders.includes(sortOrder.toLowerCase())
+      ? sortOrder.toLowerCase()
+      : "asc";
+
+    const validSortFields = ["id", "name", "region", "country"];
+    const fields = validSortFields.includes(sortBy.toLowerCase())
+      ? sortBy.toLowerCase()
+      : "id";
+
+    const institutions = await institutionRepository.findAll(
+      filters,
+      fields,
+      order,
+      page,
+      pageSize
+    );
+
+    if (!institutions.data.length) {
+      return res.status(404).json({ message: "No institutions found" });
+    }
+
+    return res.status(200).json({
+      data: institutions.data,
+      pagination: institutions.pagination,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const getInstitution = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const institution = await institutionRepository.findById(id);
+    if (!institution) {
+      return res.status(404).json({
+        message: `No institution with the id: ${id} found`,
+      });
+    }
+    return res.status(200).json({
+      data: institution,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const updateInstitution = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, region, country } = req.body;
+    let institution = await institutionRepository.findById(id);
+    if (!institution) {
+      return res.status(404).json({
+        message: `No institution with the id: ${id} found`,
+      });
+    }
+    institution = await institutionRepository.update(id, {
+      name,
+      region,
+      country,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+const deleteInstitution = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const institution = await institutionRepository.findById(id);
+    if (!institution) {
+      return res.status(404).json({
+        message: `No institution with the id: ${id} found`,
+      });
+    }
+    await institutionRepository.delete(id);
+    return res.status(200).json({
+      message: `Institution with the id: ${id} successfully deleted`,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+export {
+  createInstitution,
+  getInstitutions,
+  getInstitution,
+  updateInstitution,
+  deleteInstitution,
+};
