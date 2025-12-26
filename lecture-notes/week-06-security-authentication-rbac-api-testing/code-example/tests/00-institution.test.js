85059f357b7c235d931f37f1aaf492ade8f0e6be
Grayson Orr <grayson.orr@op.ac.nz>
2025-12-27 11:52:45 +1300

    update

--- lecture-notes/week-06-security-authentication-rbac-api-testing/code-example/tests/00-institution.test.js
+++ lecture-notes/week-06-security-authentication-rbac-api-testing/code-example/tests/00-institution.test.js
@@ -1,12 +1,33 @@
 import { expect } from "chai";
 import request from "supertest";
+
 import app from "../app.js";
-import { setupTestAuth } from "./helpers/auth.js";
+import setupTestAuth from "./helpers/auth.js";
 
 describe("Institution CRUD", () => {
+  const BASE_URL = "/api/institutions";
+
   let token;
   let institutionOneId;
   let institutionTwoId;
+
+  const institutionData = [
+    {
+      name: "Ara Institute of Canterbury",
+      region: "Canterbury",
+      country: "New Zealand",
+    },
+    {
+      name: "Otago Polytechnic",
+      region: "Otago",
+      country: "New Zealand",
+    },
+    {
+      name: "Southern Institute of Technology",
+      region: "Southland",
+      country: "New Zealand",
+    },
+  ];
 
   // Setup the test authentication before running the tests
   before(async () => {
@@ -15,70 +36,61 @@ describe("Institution CRUD", () => {
 
   it("should create institution one", async () => {
     const res = await request(app)
-      .post("/api/institutions")
+      .post(BASE_URL)
       .set("Authorization", `Bearer ${token}`) // Set the Authorization header with the token
-      .send({
-        name: "Otago Polytechnic",
-        region: "Otago",
-        country: "New Zealand",
-      });
+      .send(institutionData[1]);
 
     expect(res.status).to.equal(201);
 
     // Find an institution by name in the response body
     const newInstitution = res.body.data.find(
-      (institution) => institution.name === "Otago Polytechnic"
+      (institution) => institution.name === institutionData[1].name // "Otago Polytechnic"
     );
-    institutionOneId = newInstitution.id; // Store the institution id for later use
+    institutionOneId = newInstitution.id; // Store the institution ID for later use
   });
 
   it("should create institution two", async () => {
     const res = await request(app)
-      .post("/api/institutions")
+      .post(BASE_URL)
       .set("Authorization", `Bearer ${token}`)
-      .send({
-        name: "Southern Institute of Technology",
-        region: "Southland",
-        country: "New Zealand",
-      });
+      .send(institutionData[2]);
 
     expect(res.status).to.equal(201);
     const newInstitution = res.body.data.find(
-      (institution) => institution.name === "Southern Institute of Technology"
+      (institution) => institution.name === institutionData[2].name // "Southern Institute of Technology"
     );
     institutionTwoId = newInstitution.id;
   });
 
   it("should get all institutions", async () => {
-    const res = await request(app).get("/api/institutions");
+    const res = await request(app).get(BASE_URL);
 
     expect(res.status).to.equal(200);
     expect(res.body.data.length).to.be.at.least(2); // Check that there are at least 2 institutions
   });
 
   it("should get institution one by ID", async () => {
-    const res = await request(app).get(`/api/institutions/${institutionOneId}`);
+    const res = await request(app).get(`${BASE_URL}/${institutionOneId}`);
 
     expect(res.status).to.equal(200);
-    expect(res.body.data.name).to.equal("Otago Polytechnic");
+    expect(res.body.data.name).to.equal(institutionData[1].name); // "Otago Polytechnic"
   });
 
   it("should update institution two", async () => {
-    const res = await request(app)
-      .put(`/api/institutions/${institutionTwoId}`)
-      .send({ name: "Ara Institute of Canterbury", region: "Canterbury" });
+    const res = await request(app).put(`${BASE_URL}/${institutionTwoId}`).send({
+      name: institutionData[0].name,
+      region: institutionData[0].region,
+    });
 
     expect(res.status).to.equal(200);
     expect(res.body.message).to.equal(
       `Institution with the id: ${institutionTwoId} successfully updated`
     );
-    expect(res.body.data.name).to.equal("Ara Institute of Canterbury");
+    expect(res.body.data.name).to.equal(institutionData[0].name);
   });
 
   it("should delete institution one", async () => {
-    const res = await request(app).delete(
-      `/api/institutions/${institutionOneId}`
-    );
+    const res = await request(app).delete(`${BASE_URL}/${institutionOneId}`);
 
     expect(res.status).to.equal(200);
     expect(res.body.message).to.equal(
@@ -87,6 +99,6 @@ describe("Institution CRUD", () => {
   });
 
   after(() => {
-    global.testInstitutionId = institutionTwoId; // Store the institution id for later use in 01-department.test.js
+    global.testInstitutionId = institutionTwoId; // Store the institution ID for later use in 01-department.test.js
   });
 });
--- lecture-notes/week-06-security-authentication-rbac-api-testing/code-example/tests/01-department.test.js
+++ lecture-notes/week-06-security-authentication-rbac-api-testing/code-example/tests/01-department.test.js
@@ -1,13 +1,28 @@
 import { expect } from "chai";
 import request from "supertest";
+
 import app from "../app.js";
-import { cleanupDatabase, disconnectPrisma } from "./helpers/auth.js";
+import { cleanupDatabase, disconnectPrisma } from "./helpers/db.js";
 
 describe("Department CRUD", () => {
+  const BASE_URL = "/api/departments";
+
   let institutionId;
   let departmentOneId;
 
-  // Set up the institution id before running the tests
+  const departmentData = [
+    {
+      name: "Information Technology",
+    },
+    {
+      name: "Nursing",
+    },
+    {
+      name: "Business",
+    },
+  ];
+
+  // Set up the institution ID before running the tests
   before(async () => {
     institutionId = global.testInstitutionId;
   });
@@ -19,51 +34,47 @@ describe("Department CRUD", () => {
   });
 
   it("should create department one", async () => {
-    const res = await request(app).post("/api/departments").send({
-      name: "Information Technology",
+    const res = await request(app).post(BASE_URL).send({
+      name: departmentData[0].name,
       institutionId: institutionId,
     });
 
     expect(res.status).to.equal(201);
     const newDepartment = res.body.data.find(
-      (department) => department.name === "Information Technology"
+      (department) => department.name === departmentData[0].name // "Information Technology"
     );
     departmentOneId = newDepartment.id;
   });
 
   it("should get all departments", async () => {
-    const res = await request(app).get("/api/departments");
+    const res = await request(app).get(BASE_URL);
 
     expect(res.status).to.equal(200);
     expect(res.body.data.length).to.be.at.least(1);
   });
 
   it("should get department one by ID", async () => {
-    const res = await request(app).get(`/api/departments/${departmentOneId}`);
+    const res = await request(app).get(`${BASE_URL}/${departmentOneId}`);
 
     expect(res.status).to.equal(200);
-    expect(res.body.data.name).to.equal("Information Technology");
+    expect(res.body.data.name).to.equal(departmentData[0].name);
   });
 
   it("should update department one", async () => {
-    const res = await request(app)
-      .put(`/api/departments/${departmentOneId}`)
-      .send({
-        name: "Nursing",
-        institutionId: institutionId,
-      });
+    const res = await request(app).put(`${BASE_URL}/${departmentOneId}`).send({
+      name: departmentData[1].name,
+      institutionId: institutionId,
+    });
 
     expect(res.status).to.equal(200);
     expect(res.body.message).to.equal(
       `Department with the id: ${departmentOneId} successfully updated`
     );
-    expect(res.body.data.name).to.equal("Nursing");
+    expect(res.body.data.name).to.equal(departmentData[1].name);
   });
 
   it("should delete department one", async () => {
-    const res = await request(app).delete(
-      `/api/departments/${departmentOneId}`
-    );
+    const res = await request(app).delete(`${BASE_URL}/${departmentOneId}`);
 
     expect(res.status).to.equal(200);
     expect(res.body.message).to.equal(
