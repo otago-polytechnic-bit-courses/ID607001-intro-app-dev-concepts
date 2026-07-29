8cba0f690a7aa6ac5791c1b7933efb5e86bb629d
jgun46 <gundjp1@student.op.ac.nz>
2026-07-29 14:00:26 +1200

    format and lint check

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -2,5 +2,8 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
 ]);
